7834d5b93956e3e8d2e888138827b777af3cd2a6
Ivano Malavolta <ivanomalavolta@gmail.com>
2021-12-16 17:31:30 +0100

    Perform statistical analysis, effect size estimation, and some plotting

--- data_analysis/analysis.r
+++ data_analysis/analysis.r
@@ -2,6 +2,7 @@ setwd(".")
 library(plyr)
 library(dplyr)
 library(ggplot2)
+library(gridExtra)
 require(reshape2)
 library(nortest)
 library(effsize)
@@ -100,20 +101,20 @@ data$lvl <- as.factor(data$lvl)
 # gpu (%)
 # mem (Mb)
 
-var_names <- c('energy', 'load_time', 'fcp', 'fp', 'packets', 'bytes', 'cpu', 'gpu', 'mem')
+vars <- c('energy'='Energy (mJ)', 'load_time'='Page load time (ms)', 'fcp'='First cont. paint (ms)', 'fp'='First paint (ms)', 'packets'='HTTP requests', 'bytes'='Transferred bytes (Kb)', 'cpu'='CPU usage (%)', 'gpu'='GPU usage (%)', 'mem'='Memory usage (Mb)')
 
 # Overview of the collected data for each metric (summary statistics)
-for (v in var_names) {
+for (v in names(vars)) {
   print(summary(data[[v]]))
 }
 
 # Overview of the collected data for each metric (boxplots)
-for (v in var_names) {
+for (v in names(vars)) {
   boxplot(as.formula(paste(v, '~ lvl')), data=data)
 }
 
 # Let's see if the subjects in the wild and the subjects in the lab are very different from each other
-for (v in var_names) {
+for (v in names(vars)) {
   boxplot(as.formula(paste(v, '~ subject_type')), data=data)
 }
 
@@ -121,11 +122,11 @@ for (v in var_names) {
 data_wild <- data %>% filter(subject_type == "wild")
 data_lab <- data %>% filter(subject_type == "lab")
 
-for (v in var_names) {
+for (v in names(vars)) {
   boxplot(as.formula(paste(v, '~ lvl')), data=data_wild)
 }
 
-for (v in var_names) {
+for (v in names(vars)) {
   boxplot(as.formula(paste(v, '~ lvl')), data=data_lab)
 }
 
@@ -135,7 +136,10 @@ for (v in var_names) {
 
 # All assumptions of the Kruskal-Wallis test are met by design
 
-for (v in var_names) {
+for (v in names(vars)) {
+  
+  print(paste('-------------------', v, ' --- IN THE WILD'))
+  
   # Compute the Kruskal-Wallis test
   res.kruskal <- data_wild %>% kruskal_test(as.formula(paste(v, '~ lvl')))
   print(res.kruskal)
@@ -156,9 +160,9 @@ for (v in var_names) {
       subtitle = get_test_label(res.kruskal, detailed = TRUE),
       caption = get_pwc_label(pwc)
     ))
-}
-
-for (v in var_names) {
+  
+  print(paste('-------------------', v, ' --- IN THE LAB'))
+  
   # Compute the Kruskal-Wallis test
   res.kruskal <- data_lab %>% kruskal_test(as.formula(paste(v, '~ lvl')))
   print(res.kruskal)
@@ -181,7 +185,151 @@ for (v in var_names) {
           ))
 }
 
+#=========================================================
+# Visualizations for the article
+#=========================================================
 
+############## Plot values for all subjects (data exploration)
 
+fontSize = 9
 
+for (i in seq(1,length(vars))) {
 
+  var <- vars[i]
+  
+  # The maximum value of the scale on the Y axis is the maximum value of the metric, or 100 if the metric is a percentage
+  min_y_value <- min(data[[names(var)]])
+  max_y_value <- max(data[[names(var)]])
+  if(names(var) == 'cpu' || names(var) == 'gpu') {
+    max_y_value <- 100
+  }
+
+  bp <- ggplot(data, aes(x=subject_type, y=data[[names(var)]])) + ylim(min_y_value, max_y_value) +
+    geom_violin(trim = FALSE, alpha = 0.5, position=position_dodge(0.9)) + theme_bw() + xlab("") + ylab(var) +
+    geom_boxplot(alpha=1, color="black", width=.2, fill="white", outlier.size=0) +
+    stat_summary(fun.y=mean, colour="black", geom="point", 
+               shape=5, size=1,show_guide = FALSE) +
+    ggtitle(var) + 
+    guides(color=guide_legend(title="")) + theme(plot.title=element_text(size=fontSize), strip.text.x=element_text(size=fontSize), strip.text.y=element_text(size=fontSize),  axis.text.x=element_text(size=fontSize, angle = 45, hjust = 1), axis.text.y=element_text(size=fontSize), axis.title=element_text(size=fontSize)) +
+    scale_x_discrete(labels=c("lab" = "Lab", "wild" = "Wild")) 
+  
+  ggsave(paste('./plots/overview_wild_lab/', names(var), '.pdf', sep=''), scale = 1.6, height = 4, width = 3, unit = "cm")
+}
+
+############## Print Latex table for summarizing the overview of in-the-lab and in-the-wild subjets
+
+# Rows for all subjects
+for (i in seq(1,length(vars))) {
+  var <- vars[i]
+  stats <- data %>% summarise(min=min(data[[names(var)]]), max=max(data[[names(var)]]), median=median(data[[names(var)]]), mean=mean(data[[names(var)]]), sd=sd(data[[names(var)]])) %>%  mutate(cv=100 * sd/mean) %>% as.data.frame()
+  stats <- round(stats, 2)
+  stats_string <- paste(stats, collapse=' & ' )
+  stats_string <- (paste(var, ' &', stats_string, '\\'))
+  if(i %% 2 == 1 && i != length(vars)) {
+    stats_string <- paste(stats_string, '\rowcolor{gray10}')
+  }
+  print(stats_string)
+}
+
+# Rows for in-the-lab subjects
+for (i in seq(1,length(vars))) {
+  var <- vars[i]
+  stats <- data_lab %>% summarise(min=min(data_lab[[names(var)]]), max=max(data_lab[[names(var)]]), median=median(data_lab[[names(var)]]), mean=mean(data_lab[[names(var)]]), sd=sd(data_lab[[names(var)]])) %>%  mutate(cv=100 * sd/mean) %>% as.data.frame()
+  stats <- round(stats, 2)
+  stats_string <- paste(stats, collapse=' & ' )
+  stats_string <- (paste(var, ' &', stats_string, '\\'))
+  if(i %% 2 == 1 && i != length(vars)) {
+    stats_string <- paste(stats_string, '\rowcolor{gray10}')
+  }
+  print(stats_string)
+}
+
+# Rows for in-the-wild subjects
+for (i in seq(1,length(vars))) {
+  var <- vars[i]
+  stats <- data_wild %>% summarise(min=min(data_wild[[names(var)]]), max=max(data_wild[[names(var)]]), median=median(data_wild[[names(var)]]), mean=mean(data_wild[[names(var)]]), sd=sd(data_wild[[names(var)]])) %>%  mutate(cv=100 * sd/mean) %>% as.data.frame()
+  stats <- round(stats, 2)
+  stats_string <- paste(stats, collapse=' & ' )
+  stats_string <- (paste(var, ' &', stats_string, '\\'))
+  if(i %% 2 == 1 && i != length(vars)) {
+    stats_string <- paste(stats_string, '\rowcolor{gray10}')
+  }
+  print(stats_string)
+}
+
+############## Plot values for all RQs
+
+fontSize = 12
+
+for (i in seq(1,length(vars))) {
+  
+  var <- vars[i]
+  
+  min_y_value <- min(data[[names(var)]])
+  max_y_value <- max(data[[names(var)]])
+  if(names(var) == 'cpu' || names(var) == 'gpu') {
+    max_y_value <- 100
+  }
+  
+  filtered_by_level <- data_lab %>% filter(lvl == "0")
+  level_zero_line <- summary(filtered_by_level[[names(var)]])[['Median']]
+  
+  bp <- ggplot(data_lab, aes(x=lvl, y=data_lab[[names(var)]])) + ylim(min_y_value, max_y_value) +
+    geom_violin(trim = FALSE, alpha = 0.5, position=position_dodge(0.9)) + theme_bw() + xlab("") + ylab(var) +
+    geom_boxplot(alpha=1, color="black", width=.2, fill="white", outlier.size=0) +
+    stat_summary(fun.y=mean, colour="black", geom="point", 
+                 shape=5, size=1,show_guide = FALSE) +
+    ggtitle('In-the-lab subjects') +
+    geom_hline(yintercept=level_zero_line, linetype="dashed", color = "red") +
+    guides(color=guide_legend(title="")) + theme(plot.title=element_text(size=fontSize), strip.text.x=element_text(size=fontSize), strip.text.y=element_text(size=fontSize),  axis.text.x=element_text(size=fontSize, angle = 45, hjust = 1), axis.text.y=element_text(size=fontSize), axis.title=element_text(size=fontSize)) +
+    scale_x_discrete(labels=c("0" = "OL-0", "1" = "OL-1", "2" = "OL-2", "3" = "OL-3"))
+  
+  ggsave(paste('./plots/', names(var), '_lab.pdf', sep=''), scale = 1.6, height = 6, width = 4, unit = "cm")
+  
+  filtered_by_level <- data_wild %>% filter(lvl == "0")
+  level_zero_line <- summary(filtered_by_level[[names(var)]])[['Median']]
+  
+  bp <- ggplot(data_wild, aes(x=lvl, y=data_wild[[names(var)]])) + ylim(min_y_value, max_y_value) +
+    geom_violin(trim = FALSE, alpha = 0.5, position=position_dodge(0.9)) + theme_bw() + xlab("") + ylab('') +
+    geom_boxplot(alpha=1, color="black", width=.2, fill="white", outlier.size=0) +
+    stat_summary(fun.y=mean, colour="black", geom="point", 
+                 shape=5, size=1,show_guide = FALSE) +
+    ggtitle('In-the-wild subjects') +
+    geom_hline(yintercept=level_zero_line, linetype="dashed", color = "red") +
+    guides(color=guide_legend(title="")) + theme(plot.title=element_text(size=fontSize), strip.text.x=element_text(size=fontSize), strip.text.y=element_text(size=fontSize),  axis.text.x=element_text(size=fontSize, angle = 45, hjust = 1), axis.text.y=element_text(size=fontSize), axis.title=element_text(size=fontSize)) +
+    scale_x_discrete(labels=c("0" = "OL-0", "1" = "OL-1", "2" = "OL-2", "3" = "OL-3"))
+  
+  ggsave(paste('./plots/', names(var), '_wild.pdf', sep=''), scale = 1.6, height = 6, width = 4, unit = "cm")
+}
+
+############## Print Latex tables for summarizing the data collected for each RQ
+
+for (i in seq(1,length(vars))) {
+  var <- vars[i]
+  print(paste('\\multicolumn{7}{c}{\textbf{', var, '}} \\ ', sep=''))
+  lvls <- levels(data$lvl)
+  print('LAB data')
+  for(j in lvls) {
+    current_data <- data_lab %>% filter(lvl == j)
+    stats <- current_data %>% summarise(min=min(current_data[[names(var)]]), max=max(current_data[[names(var)]]), median=median(current_data[[names(var)]]), mean=mean(current_data[[names(var)]]), sd=sd(current_data[[names(var)]])) %>%  mutate(cv=100 * sd/mean) %>% as.data.frame()
+    stats <- round(stats, 2)
+    stats_string <- paste(stats, collapse=' & ' )
+    stats_string <- (paste('OL-', j, ' & ', stats_string, ' \\ ', sep=''))
+    if(j == '0' && j == '2') {
+      stats_string <- paste(stats_string, '\rowcolor{gray10}')
+    }
+    print(stats_string) 
+  }
+  print('WILD data')
+  for(j in lvls) {
+    current_data <- data_wild %>% filter(lvl == j)
+    stats <- current_data %>% summarise(min=min(current_data[[names(var)]]), max=max(current_data[[names(var)]]), median=median(current_data[[names(var)]]), mean=mean(current_data[[names(var)]]), sd=sd(current_data[[names(var)]])) %>%  mutate(cv=100 * sd/mean) %>% as.data.frame()
+    stats <- round(stats, 2)
+    stats_string <- paste(stats, collapse=' & ' )
+    stats_string <- (paste('OL-', j, ' & ', stats_string, ' \\ ', sep=''))
+    if(j == '0' && j == '2') {
+      stats_string <- paste(stats_string, '\rowcolor{gray10}')
+    }
+    print(stats_string) 
+  }
+}
