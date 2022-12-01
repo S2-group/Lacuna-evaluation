3c3d6ef6b2d2414eb39e86889f3079eb8aa6bad5
Ivano Malavolta <ivanomalavolta@gmail.com>
2022-12-01 17:48:23 +0100

    Add scatterplots (TODO: refine creation of df for Kendall's tau results

--- data_analysis/correlation_analysis.r
+++ data_analysis/correlation_analysis.r
@@ -15,6 +15,7 @@ library(ez)
 library(bestNormalize)
 library(effectsize)
 library(rmcorr)
+library(outliers)
 
 options(max.print=50)
 font_size = 10
@@ -70,7 +71,7 @@ data = rename(data, mem = Memory.Usage..KB.)
 ############################
 
 # We remove Amazon.in since we already have Amazon.com
-data <- data %>% filter(subject_name != "www-amazon-in")
+data <- data %>% filter(subject_name != "www.amazon.in")
 data$subject_name <- factor(data$subject_name)
 
 # We remove 5 (randomly identified) in-the-lab subjects in order to have a balanced comparison with the in-the-wild subjects (15 vs 15)
@@ -115,15 +116,38 @@ vars <- c('energy'='Energy (mJ)', 'load_time'='Page load time (ms)', 'fcp'='Firs
 
 # Let's compute the within subject coefficient of variation (so to decide if we can use the average for aggregating the 20 repeated measures)
 
-for (v in names(vars)) {
-  print(v)
-  my.rmc <- rmcorr(participant = subject_name, measure1 = v, measure2 = dead, dataset = all_data)
-  print(my.rmc)
-  plot(my.rmc)
+check_normality = function(data, var_name) {
+  print(var_name)
+  plot(density(data), main=var_name) 
+  boxplot(data, main=var_name) 
+  qqPlot(data, main=var_name)
+  print(shapiro.test(data))
 }
 
-my.rmc <- rmcorr_mat(participant = subject_name, variables=names(vars), dataset = all_data)
-print(my.rmc)
+correlation_results <- list()
+columns = c("var","lvl","p","tau")
+corr_result = data.frame(matrix(nrow = 0, ncol = length(columns)))
+colnames(corr_result) = columns
+for (v in names(vars)) {
+  for (i in levels(all_data$lvl)) {
+    print(paste(c("Current variable:", v, "Level: ", i)))
+    current_data <- all_data %>% filter(lvl == i)
+    current_data <- current_data %>% group_by(subject_name, lvl) %>% summarise_at(vars(v, dead), mean)
+
+    corr <- cor.test(current_data[[v]], current_data$dead, method="kendall")
+    corr_result <- corr_result %>% add_row(var=33, lvl=0, p = 5, tau = 5)
+    correlation_results <- append(correlation_results, corr_result)
+  }
+  to_plot_data <- all_data %>% group_by(subject_name, lvl) %>% summarise_at(vars(v, dead), mean)
+  current_plot <- ggplot(to_plot_data, aes(x=.data[[v]], y=dead, color=lvl)) + 
+    geom_point(alpha=0.5) + 
+    theme_bw() + 
+    theme(legend.position = c(0.90, 0.70)) +
+    labs(x= vars[[v]], y="Number of dead JS functions") + 
+    geom_smooth(method=lm,se = FALSE, size=0.5) +
+    guides(color=guide_legend(title="OL"))
+  ggsave(paste('./plots/scatterplots/', v, '.pdf', sep=''), scale = 2, height = 4, width = 6, unit = "cm")
+}
 
 # ############## Plot values for all subjects (data exploration)
 # 
